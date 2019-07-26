9b8f94fb79b09d54da60fa9b6c945f012ca67b2e
George Vince <George.Vince@hotmail.co.uk>
2019-07-26 17:01:56 +0100

    feat: began work on constructor for StageToRedshift Operator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,25 +1,40 @@
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_hook import AwsHook
+
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
+                 s3_bucket = '',
+                 s3_key = '',
+                 aws_conn_id = '',
+                 redshift_conn_id ='',
+                 table='',
+                 create_query='',
+                 copy_query='',
+                 *args, **kwargs)
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3.s3_key
+        self.aws_conn_id = aws.aws_conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.create_query = create_query
+        self.copy_query = copy_query
+
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
+        aws_hook = AwsHook(self.aws_conn_id)
 
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Dropping {} from redshift".format(self.table))
+        redshift.run("drop table if exists {}".format(self.table))
 
 
 
