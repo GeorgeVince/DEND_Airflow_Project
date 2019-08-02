47f0e8f03ad0510a2cc516ba6f79e0f62af2425d
George Vince <George.Vince@hotmail.co.uk>
2019-08-02 08:58:22 +0100

    feat: Added time table to creation statements

--- dags/create_tables.sql
+++ dags/create_tables.sql
@@ -84,3 +84,15 @@ CREATE TABLE IF NOT EXISTS public.users (
 	CONSTRAINT users_pkey PRIMARY KEY (userid)
 );
 
+CREATE TABLE public."time" (
+	start_time timestamp NOT NULL,
+	"hour" int4,
+	"day" int4,
+	week int4,
+	"month" varchar(256),
+	"year" int4,
+	weekday varchar(256),
+	CONSTRAINT time_pkey PRIMARY KEY (start_time)
+);
+
+
